87c326e79b9efae34e5efe845d41e46dea586b69
Daniel Vega Marvez <87838943+Chuz047@users.noreply.github.com>
2024-06-09 20:38:43 -0600

    Semana 3.

--- View/registro.php
+++ View/registro.php
@@ -1,9 +1,11 @@
+<?php include_once '../Controller/usuarioController.php'; ?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Registration Page</title>
+  <title>Sistema Web</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
@@ -13,59 +15,55 @@
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-  <a href="../../index2.html">Sistema Web</a>
+  <a>Sistema Web</a>
   </div>
 
   <div class="card">
     <div class="card-body register-card-body">
-      <p class="login-box-msg">Register a new membership</p>
+      <p class="login-box-msg">Registro de Usuarios</p>
 
-      <form action="../../index.html" method="post">
+      <form action="" method="post">
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Full name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+            <input id="txtIdentificacion" name="txtIdentificacion" type="text" class="form-control" placeholder="Identificación" 
+              onkeyup="ConsultarNombre();" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+          <div class="input-group mb-3">
+            <input id="txtNombre" name="txtNombre" type="text" readOnly class="form-control" placeholder="Nombre" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="email" id="txtEmail" name="txtEmail" class="form-control" placeholder="Correo Electrónico" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="password" id="txtPassword" name="txtPassword" class="form-control" placeholder="Contraseña" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-              <label for="agreeTerms">
-               I agree to the <a href="#">terms</a>
-              </label>
+          <div class="row">
+            <div class="col-7">
+              
+            </div>
+            <div class="col-5">
+              <button type="submit" id="btnProcesar" name="btnProcesar" disabled class="btn btn-primary btn-block">Procesar</button>
             </div>
           </div>
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
-          </div>
-        </div>
       </form>
 
       <a href="login.php" class="text-center">Iniciar sesión</a>
@@ -76,5 +74,6 @@
 <script src="plugins/jquery/jquery.min.js"></script>
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="dist/js/adminlte.min.js"></script>
+<script src="dist/js/usuarios.js"></script>
 </body>
 </html>
