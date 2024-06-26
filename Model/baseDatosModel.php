cd7593bc89593a45009b5a79c5ce88fdac181739
Daniel Vega Marvez <87838943+Chuz047@users.noreply.github.com>
2024-06-26 13:11:28 -0600

    Semana 5.

--- Model/baseDatosModel.php
+++ Model/baseDatosModel.php
@@ -1,13 +1,13 @@
 <?php
 
-    function AbrirBaseDatos(){
-
-        return mysqli_connect('localhost', 'root', '', 'miercoles_bd');
+    function AbrirBaseDatos()
+    {
+        return mysqli_connect('127.0.0.1:3309', 'root', '', 'miercoles_bd');
     }
 
-    function CerrarBaseDatos($conexion){
-
+    function CerrarBaseDatos($conexion)
+    {
         mysqli_close($conexion);
     }
-    
+
 ?>
